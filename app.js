fb7a891b9be9e5b62d56e3b44179f9cc956f12f7
PEdbot <vinicius.tamae@pedbot.com.br>
2023-12-15 13:33:58 -0300

    calculator-work

--- app.js
+++ app.js
@@ -7,10 +7,26 @@ const clear = document.querySelector('#c')
 
 numberButtons.forEach(function (numberButton) {
     numberButton.addEventListener('click', function () {
-        screen.value += numberButton;
+        const number = this.textContent;
+        screen.value += number;
+
+    });
+});
+
+charSpecialButtons.forEach(function (specialButton) {
+    specialButton.addEventListener('click', function () {
+        const special = this.textContent;
+        screen.value += special;
+
     });
 });
 
 clear.addEventListener('click', () => {
     screen.value = ''
+})
+
+sendButton.addEventListener('click', () => {
+    let result = eval(screen.value)
+    console.log(result)
+    screen.value = result
 })
