5bac32da32a72c09f91520cc3930d080f6528d6a
PEdbot <vinicius.tamae@pedbot.com.br>
2023-12-19 18:00:48 -0300

    Substituindo Resultado

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const numberButtons = document.querySelectorAll('#number');
-const charSpecialButtons = document.querySelectorAll('#charSpecial');
-const sendButton = document.querySelector('#send');
-const clear = document.querySelector('#c')
-const backSpace = document.querySelector('#backSpace')
+const numberButtons = document.querySelectorAll('.number');
+const charSpecialButtons = document.querySelectorAll('.charSpecial');
+const sendButton = document.querySelector('.send');
+const clear = document.querySelector('#deleteAll')
+const backSpace = document.querySelector('#delete')
 const screen = document.querySelector('h2')
 
 
 numberButtons.forEach(function (numberButton) {
     numberButton.addEventListener('click', function () {
         const number = this.textContent;
-        screen.textContent = number;
+        if (screen.textContent == '0') {
+            screen.textContent = number;
+        } else {
+            screen.textContent += number;
+        }
     });
 });
 
@@ -17,6 +21,7 @@ charSpecialButtons.forEach(function (specialButton) {
     specialButton.addEventListener('click', function () {
         const special = this.textContent;
         screen.textContent += special;
+
     });
 });
 
@@ -25,10 +30,11 @@ clear.addEventListener('click', () => {
 })
 
 sendButton.addEventListener('click', () => {
-    let result = eval(screen.innerText)
-    console.log(result)
+    let result = eval(screen.innerText);
+    console.log(result);
     screen.innerText = result
-})
+});
+
 
 backSpace.addEventListener('click', () => {
     screen.innerText = screen.innerText.slice(0, -1);
@@ -36,13 +42,18 @@ backSpace.addEventListener('click', () => {
 
 window.addEventListener('keydown', (e) => {
     if (!isNaN(e.key) || ['+', '-', '*', '/'].includes(e.key)) {
-        screen.innerText += e.key;
+        if (screen.textContent == '0') {
+            screen.innerText = e.key;
+        } else {
+            screen.innerText += e.key;
+        }
     } else if (e.key === 'Enter') {
         sendButton.click();
     } else if (e.key === 'c') {
-        screen.innerText = ''
+        screen.innerText = '0'
     } else if (e.key === 'Backspace') {
         screen.innerText = screen.innerText.slice(0, -1);
     }
 })
+
 
